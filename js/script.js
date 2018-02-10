f400ffc35cc7f47738589b238c7ac5d1258f0366
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-02-10 04:21:38 +0100

    Define game states && add listeners to key elements

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ var gameState = 'notStarted',  //started // ended
     computer = {
         score: 0
     };
+var newGameElem = document.getElementById('js-newGameElement'),
+	pickElem = document.getElementById('js-playerPickElement'),
+	resultsElem = document.getElementById('js-resultsTableElement');
+	function setGameElements() {
+  switch(gameState) {
+    case 'started':
+        newGameElem.style.display = 'none';
+        pickElem.style.display = 'block';
+        resultsElem.style.display = 'block';
+      break;
+    case 'ended':
+        newGameBtn.innerText = 'Play again';
+    case 'notStarted':
+    default:
+        newGameElem.style.display = 'block';
+        pickElem.style.display = 'none';
+        resultsElem.style.display = 'none';
+  }
+}
