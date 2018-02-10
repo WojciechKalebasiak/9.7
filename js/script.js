51eb738536c810b6f1d39845e9999982d2d216d7
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-02-10 04:29:48 +0100

    Add new game function && player&computer pick

--- js/script.js
+++ js/script.js
@@ -35,3 +35,24 @@ var newGameElem = document.getElementById('js-newGameElement'),
         resultsElem.style.display = 'none';
   }
 }
+var playerPointsElem = document.getElementById('js-playerPoints'),
+    playerNameElem = document.getElementById('js-playerName'),
+    computerPointsElem = document.getElementById('js-computerPoints');
+function newGame() {
+  player.name = prompt('Please enter your name', 'imię gracza');
+  if (player.name) {
+    player.score = computer.score = 0;
+    gameState = 'started';
+    setGameElements();
+
+    playerNameElem.innerHTML = player.name;
+  }
+
+}
+function playerPick(playerPick) {
+    console.log(playerPick);
+}
+function getComputerPick() {
+    var possiblePicks = ['rock', 'paper', 'scissors'];
+    return possiblePicks[Math.floor(Math.random()*3)];
+}
